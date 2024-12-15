f456541ad09b967e1beeca366a074ac2d6629dea
prajjwalsoni123 <prajjwalsoni123@gmail.com>
2024-12-15 13:48:56 +0530

    headers changes

--- src/app/dashboard/_components/Header.tsx
+++ src/app/dashboard/_components/Header.tsx
@@ -5,8 +5,22 @@ import { UserButton } from '@clerk/nextjs';
 
 const Header = () => {
   return (
-    <div className='flex p-4 justify-between'>
+    <div className='flex p-4 justify-between items-center'>
       <UserButton/>
+      <ul className='flex justify-between gap-4'>
+        <li>
+          Dashboard
+        </li>
+        <li>
+          Questions
+        </li> 
+        <li>
+          Content
+        </li>
+        <li>
+          How it works
+        </li>
+      </ul>
       <Image src={logoI.src} alt='' height={20} width={20}/>   
     </div>
   )
